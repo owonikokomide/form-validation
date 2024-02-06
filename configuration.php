a61c58fbc8ad1584d6c39d5aee2570b1ac7f2f3e
owonikoko Ayomide <donaykoko@yahoo.com>
2024-02-06 14:30:44 +0100

    Update configuration.php

--- configuration.php
+++ configuration.php
@@ -6,7 +6,7 @@ define("dbname", "second_project");
 
 $connection = mysqli_connect(servername, username, password, dbname);
 if($connection){
-  echo "connected";
+  echo "connected"; 
 }
 else{
   die ("not-connected").mysqli_connect_errno;
