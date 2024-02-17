e21e067e434794731821fcf697fb78120f537b96
owonikoko Ayomide <donaykoko@yahoo.com>
2024-02-17 12:53:24 +0100

    Update success.php

--- success.php
+++ success.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 session_start();
 // $text = $_GET['name'];
 echo "<marquee scrollamount='15' behavior='alternate' style='color:green';> <h1>Congratulations " . $_SESSION['name'], ", Welcome To Your Dashboard</h1></marquee>";
