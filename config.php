a9ad6396af897b52af7ee46f0a2f8cd6bbcacfec
owonikoko Ayomide <donaykoko@yahoo.com>
2024-03-08 12:43:36 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = ""; 
 $dbname = "first_project"; 
 
 $connection = mysqli_connect($servername, $username, $password, $dbname);
